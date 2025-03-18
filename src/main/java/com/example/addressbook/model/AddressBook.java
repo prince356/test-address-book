a92643b6cb32bac5876950197222bfcf1d8649cf
prince356 <princechaurasia014@gmail.com>
2025-03-18 22:00:16 +0530

    UC5: Added Lombok to DTO and Model

--- src/main/java/com/example/addressbook/model/AddressBook.java
+++ src/main/java/com/example/addressbook/model/AddressBook.java
@@ -1,49 +1,15 @@
 package com.example.addressbook.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class AddressBook {
     private int id;
     private String name;
     private String email;
     private String phoneNumber;
-
-    public AddressBook() {}
-
-    public AddressBook(int id, String name, String email, String phoneNumber) {
-        this.id = id;
-        this.name = name;
-        this.email = email;
-        this.phoneNumber = phoneNumber;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPhoneNumber() {
-        return phoneNumber;
-    }
-
-    public void setPhoneNumber(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
-    }
 }
